69162ac27d99b70e21a31c0dbc93327501605f79
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-06-15 15:44:03 -0300

    Aula JavaScript

---
+++ Modulo 3 Aulas de JavaScript/Aula 5 - JavaScript Estrutura Função/script.js
@@ -0,0 +1,12 @@
+/*/FUNÇÕES
+
+function funcao() {
+    console.log('Essa mensagem veio de uma função!')
+}
+funcao();*/
+
+//FUNÇÕES COM PARAMETROS
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+mensagem('Tudo cero', 'Jovem');
---
+++ Modulo 3 Aulas de JavaScript/Aula 6 - JavaScript Aprofundando em Função/tiposdeFuncao.js
@@ -0,0 +1,21 @@
+//Tipos de função
+
+/*Declarativas
+function funcao() {
+    console.log('Sou uma mensagem de uma função declarativa')
+}
+funcao();*/
+
+/*/ expressão de função
+//com nomeação
+var funcao = function() {
+    console.log('Sou uma mensagem de função de expresão!')
+}
+funcao();*/
+
+//arrow funcion
+var fucao = ()=> {
+    console.log('Sou uma arrow function')
+}
+fucao();
+
